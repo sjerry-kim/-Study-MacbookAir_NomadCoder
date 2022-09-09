015bf1f3db75d105f120536acf688893b1878331
jinhye <sjrry112@gmail.com>
2022-09-09 15:29:11 +0900

    220909_1st_Commit

---
+++ JSpractice01/index.js
@@ -0,0 +1,29 @@
+String
+const what = "Nicolas";
+
+Boolean
+const wat = true;
+
+Number
+//const wat =666;
+
+float
+//const wat= 55.1;
+
+/*const calculator = {
+    plus : function(a, b){
+        return a + b;
+    }
+}
+
+const plus = calculator.plus(5, 5)
+console.log(plus)*/
+
+const calculator = {
+    mul: function(a, b){
+        return a * b;
+    }
+}
+
+const mul = calculator.mul(5, 5)
+console.log(mul)
---
+++ JSpractice01_copy/gretting.js
@@ -0,0 +1,24 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+
+function paintingGreeting(text){
+
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem()
+    if(currentUser===null){
+
+    }else{
+
+    }
+}
+
+function init(){
+    loadName();
+}
+
+init();
